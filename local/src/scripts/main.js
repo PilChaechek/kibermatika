446020e54e5369de9abac8dfacd30b84df59a25b
Gleb Neverov <hellhands@yandex.ru>
2026-02-12 15:10:05 +0700

    feat: Новая страница "Киберматы" Блоки: Hero, Видео, Карусель, Header, Footer

--- local/src/scripts/main.js
+++ local/src/scripts/main.js
@@ -14,27 +14,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 // Активный пункт для меню
-document.addEventListener('DOMContentLoaded', () => {
-	const nav = document.querySelector('.main-nav');
-
-	if (!nav) return;
-
-	nav.addEventListener('click', (event) => {
-		const targetItem = event.target.closest('.main-nav__item');
-
-		if (!targetItem || targetItem.classList.contains('main-nav__item--active')) {
-			return;
-		}
-
-		event.preventDefault();
-
-		const activeItem = nav.querySelector('.main-nav__item--active');
-		if (activeItem) {
-			activeItem.classList.remove('main-nav__item--active');
-		}
-
-		targetItem.classList.add('main-nav__item--active');
-
-	});
-});
+// document.addEventListener('DOMContentLoaded', () => {
+// 	const nav = document.querySelector('.main-nav');
+//
+// 	if (!nav) return;
+//
+// 	nav.addEventListener('click', (event) => {
+// 		const targetItem = event.target.closest('.main-nav__item');
+//
+// 		if (!targetItem || targetItem.classList.contains('main-nav__item--active')) {
+// 			return;
+// 		}
+//
+// 		event.preventDefault();
+//
+// 		const activeItem = nav.querySelector('.main-nav__item--active');
+// 		if (activeItem) {
+// 			activeItem.classList.remove('main-nav__item--active');
+// 		}
+//
+// 		targetItem.classList.add('main-nav__item--active');
+//
+// 	});
+// });
 
